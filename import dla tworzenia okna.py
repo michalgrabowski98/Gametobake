d4d935fc630a949a4d2cdb801928a38612ed5c44
Michał <114815080+michalgrabowski98@users.noreply.github.com>
2025-01-08 00:02:53 +0100

    Stworzenie gifa, kręci się dobrze, dobrze jest, dobrze chłopaki robią

--- import dla tworzenia okna.py
+++ import dla tworzenia okna.py
@@ -1,11 +1,22 @@
-from tkinter import Tk, Button
+
+#tkinter dla tworzenia okna tekstów i guzików etc.
+from tkinter import *
 
 root = Tk()
+#rozmiar okna
 root.geometry("900x300")
+
+#nazwa okna
 root.title("TEST")
 
+#tekst w oknie
+text_label = Label(root, bg="red", text= "piwooooooo, kocham piwoooooooooo")
+
+#guzior zamknięcia
 buttonexit = Button(root, text = "Exit", command = root.destroy) 
 
 
+#żeby się ładowało i działało
+text_label.pack()
 buttonexit.pack()
 root.mainloop()
