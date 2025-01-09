4da65fb2947ea67130c6d9f8861639bc00602534
Michał <114815080+michalgrabowski98@users.noreply.github.com>
2025-01-09 23:43:16 +0100

    dialogi i gra generalnie wyświetla się na display

--- testowydisplayout.py
+++ testowydisplayout.py
@@ -3,14 +3,56 @@ from tkinter import *
 
 #wprowadzanie imiona
 def nameofplayer():
+    global name
     name = txt.get()
     #if sprawdza czy coś zostało wprowadzone
     if name:
-        lbl.configure (text = f"Hello {name}")
-        btn.configure (text = "Next", command= answer)
+        lbl.configure (text = f"Hello {name}, you wanna play same game (yes/no)")
+        #guzik przenosi do kolejnych opcji
+        btn.configure (text = "Next", command = answer1)
+    
     #delete czyści pole tekstowe txt od 0 - początek do END - koniec
     txt.delete(0, END)
-    return nameofplayer
+    return name
+    
+ 
+#otwarcie gry, def2
+def answer1():
+    answer = txt.get().lower().strip()
+
+
+    if answer == "yes":
+        lbl.configure (text=("You have reached doors to big crypt. It's kinda strange to see also some ring to it, they have eletricity down there?"))
+        btn.configure (text = "Next", command= answer2)
+        txt.delete(0, END)  
+        #koniec gry
+    else:
+        btn.configure (text="Exit", command= root.destroy)
+        lbl.configure (text="please press exit to close the game")
+        txt.delete(0, END)       
+
+
+def answer2():
+    answer = txt.get()
+
+    if answer == "knock":
+        lbl.configure (text= ("The old woman, around here 30s," 
+                              "opened the door and looks at you with her dark brown eyes -"
+                                f"What is your wish to find here {name}?(tea/coffe))")
+                        )
+        btn.configure (text="Next", command=answer3)
+        txt.delete(0, END)
+
+
+def answer3():
+    answer = txt.get().lower().strip()
+
+    if answer == "x":
+        lbl.configure(text="test")
+
+
+
+
 
 #tworzenie okna
 root= Tk()
@@ -36,9 +78,6 @@ btn = Button(root, text = "Enter", command= nameofplayer)
 
 #pozycja guziora
 btn.grid(column=3, row=1)
-
-
-
 
 
 
